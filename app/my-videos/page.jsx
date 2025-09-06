32dac8172827d2d9bdae2646993bd95dbd2994a9
Priyanshi <priyanshi.dubey.csbs22@ggits.net>
2025-09-06 09:17:46 +0530

    Resolved conflicts by keeping my local code

--- app/my-videos/page.jsx
+++ app/my-videos/page.jsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import MyVideos from "../../components/my-videos"
 
 export default function Page() {
   return <MyVideos />
 }
-=======
-import MyVideos from "../../components/my-videos"
-
-export default function Page() {
-  return <MyVideos />
-}
->>>>>>> 8a252175bc6bdeefea69a93467562e838835259e
--- components/ui/toaster.jsx
+++ components/ui/toaster.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 
 import { useToast } from "@/hooks/use-toast"
@@ -23,29 +22,3 @@ export function Toaster() {
     </ToastProvider>
   )
 }
-=======
-"use client"
-
-import { useToast } from "@/hooks/use-toast"
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast"
-
-export function Toaster() {
-  const { toasts } = useToast()
-
-  return (
-    <ToastProvider>
-      {toasts.map(({ id, title, description, action, ...props }) => (
-        <Toast key={id} {...props}>
-          <div className="grid gap-1">
-            {title && <ToastTitle>{title}</ToastTitle>}
-            {description && <ToastDescription>{description}</ToastDescription>}
-          </div>
-          {action}
-          <ToastClose />
-        </Toast>
-      ))}
-      <ToastViewport />
-    </ToastProvider>
-  )
-}
->>>>>>> 8a252175bc6bdeefea69a93467562e838835259e
